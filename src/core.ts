dd23cac9f8bedcea27c0707963161d77e920dd8b
kimcat <79156711+kimjaehyeon221@users.noreply.github.com>
2026-09-01 18:23:23 +0900

    Point app help links to dedicated release support site

--- src/core.ts
+++ src/core.ts
@@ -23,8 +23,8 @@ export type AppState = {
 export const STORAGE_KEY = 'baemilgi2000-ios-v1';
 export const FORM_VIDEO_URL = 'https://www.youtube.com/watch?v=8zuZ_Ybe8nc';
 export const GAMA_SOURCE_URL = 'https://simplexstrong.com/2020/03/what-makes-the-oriental-strong-the-indian-dands-1911/';
-export const PRIVACY_URL = 'https://baemilgi2000-upendjh-6028s-projects.vercel.app/privacy.html';
-export const SUPPORT_URL = 'https://baemilgi2000-upendjh-6028s-projects.vercel.app/support.html';
+export const PRIVACY_URL = 'https://baemilgi2000-support-upendjh-6028s-projects.vercel.app/privacy';
+export const SUPPORT_URL = 'https://baemilgi2000-support-upendjh-6028s-projects.vercel.app/support';
 
 export const initialState: AppState = {
   onboarded: false,
